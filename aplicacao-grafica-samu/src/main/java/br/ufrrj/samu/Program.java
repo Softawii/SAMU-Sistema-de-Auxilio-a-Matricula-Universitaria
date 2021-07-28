6f4118f4c042988084561ea56b4d4a7710bd6c4e
Eduardo Cabral <47820549+FerroEduardo@users.noreply.github.com>
2021-07-28 10:51:50 -0300

    Remove unnecessary instructions from Program.java

--- aplicacao-grafica-samu/src/main/java/br/ufrrj/samu/Program.java
+++ aplicacao-grafica-samu/src/main/java/br/ufrrj/samu/Program.java
@@ -1,21 +1,7 @@
 package br.ufrrj.samu;
 
-import br.ufrrj.samu.entities.User;
-import br.ufrrj.samu.services.UserService;
-import org.apache.logging.log4j.LogManager;
-import org.apache.logging.log4j.Logger;
-
 public class Program {
     public static void main(String[] args) {
         SAMU.startSamu();
-        // SAMU.startSamu();
-
-        Logger LOGGER = LogManager.getLogger(UserService.class);
-
-        UserService userService = new UserService();
-
-        userService.insertUser(new User("Joao", "1234"));
-
-        LOGGER.debug(userService.checkPassword("Joao", "1234"));
     }
 }
